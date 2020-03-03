2a1d613e6ed270a3063266267d1c3bb3bc2ba0b5
brandonschultz <baschultz2842@eagle.fgcu.edu>
2020-03-03 13:27:16 -0500

    Added another test for set() method

--- GUI/AddressBookTest.java
+++ GUI/AddressBookTest.java
@@ -21,6 +21,11 @@ class AddressBookTest {
 
   @Test
   void set() {
+    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
+    persons.add(p);
+    Person p2 = new Person("test2","test","123 Test", "Test", "Test", "Test", "Test");
+    persons.set(0,p2);
+    assertEquals("test2",persons.get(0).getFirstName());
   }
 
   @Test
