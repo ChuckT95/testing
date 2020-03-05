39ce500e4b3c58a235c80821e2986d964942aa47
brandonschultz <baschultz2842@eagle.fgcu.edu>
2020-03-05 09:53:40 -0500

    All test methods in PersonTest should be finished

--- GUI/PersonTest.java
+++ GUI/PersonTest.java
@@ -2,56 +2,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 import java.util.regex.Pattern;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PersonTest {
 
+  private Person TestingPerson;
+
+  @BeforeEach
+  void setUp(){
+    TestingPerson = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
+  }
+
   @Test
   void getFirstName() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String firstName = p.getFirstName();
+    //
+    String firstName = TestingPerson.getFirstName();
     assertEquals("test",firstName);
   }
 
   @Test
   void getLastName() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String lastName = p.getLastName();
+    
+    String lastName = TestingPerson.getLastName();
     assertEquals("test",lastName);
   }
 
   @Test
   void getAddress() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String address = p.getAddress();
+    
+    String address = TestingPerson.getAddress();
     assertEquals("123 Test",address);
   }
 
   @Test
   void getCity() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String city = p.getCity();
+    
+    String city = TestingPerson.getCity();
     assertEquals("Test",city);
   }
 
   @Test
   void getState() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String state = p.getState();
+    
+    String state = TestingPerson.getState();
     assertEquals("Test",state);
   }
 
   @Test
   void getZip() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String zip = p.getZip();
+    
+    String zip = TestingPerson.getZip();
     assertEquals("Test",zip);
   }
 
   @Test
   void getPhone() {
-    Person p = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
-    String phone = p.getPhone();
+    
+    String phone = TestingPerson.getPhone();
     assertEquals("Test",phone);
 
   }
@@ -63,22 +73,50 @@ class PersonTest {
   @Test
   void containsString() {
     String test = "test";
-    Pattern p = Pattern.compile(Pattern.quote(test), Pattern.CASE_INSENSITIVE);
+    Pattern pa = Pattern.compile(Pattern.quote(test), Pattern.CASE_INSENSITIVE);
     Person person = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
     boolean found =
-        p.matcher(person.getFirstName()).find()
-            || p.matcher(person.getLastName()).find()
-            || p.matcher(person.getAddress()).find()
-            || p.matcher(person.getCity()).find()
-            || p.matcher(person.getState()).find()
-            || p.matcher(person.getZip()).find()
-            || p.matcher(person.getPhone()).find();
+        pa.matcher(person.getFirstName()).find()
+            || pa.matcher(person.getLastName()).find()
+            || pa.matcher(person.getAddress()).find()
+            || pa.matcher(person.getCity()).find()
+            || pa.matcher(person.getState()).find()
+            || pa.matcher(person.getZip()).find()
+            || pa.matcher(person.getPhone()).find();
     assertTrue(found);
 
   }
 
-
-  void getField() {
-
+  @ParameterizedTest
+  @ValueSource(ints = { -1,0,1,2,3,4,5,6,7 })
+  void getField(int field) {
+    
+    switch (field) {
+      case 0:
+        assertEquals("test",TestingPerson.getFirstName());
+        break;
+      case 1:
+        assertEquals("test",TestingPerson.getLastName());
+        break;
+      case 2:
+        assertEquals("123 Test",TestingPerson.getAddress());
+        break;
+      case 3:
+        assertEquals("Test",TestingPerson.getCity());
+        break;
+      case 4:
+        assertEquals("Test",TestingPerson.getState());
+        break;
+      case 5:
+        assertEquals("Test",TestingPerson.getZip());
+        break;
+      case 6:
+        assertEquals("Test",TestingPerson.getPhone());
+        break;
+      default:
+        assertThrows(IllegalArgumentException.class, () -> {
+          TestingPerson.getField(field);
+        });
+    }
   }
 }
