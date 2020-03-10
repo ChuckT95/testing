de113ae66f2af28e22adb84d7734e9319ff733f3
brandonschultz <baschultz2842@eagle.fgcu.edu>
2020-03-09 20:58:34 -0400

    Added comments to both Test classes

--- GUI/AddressBookTest.java
+++ GUI/AddressBookTest.java
@@ -14,6 +14,8 @@ class AddressBookTest {
   private Person TestingPerson2;
 
 
+  //Execute this method before each test method is executed
+  //This creates two new Person Objects
   @BeforeEach
   void setUp(){
     TestingPerson = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
@@ -23,38 +25,42 @@ class AddressBookTest {
 
   @Test
   void getPersons() {
-    persons.add(TestingPerson);
-    persons.add(TestingPerson2);
-    Person[] pArr = new Person[] {TestingPerson,TestingPerson2};
-    assertArrayEquals(pArr,persons.toArray());
+    persons.add(TestingPerson); //Add person to list
+    persons.add(TestingPerson2); //Add another person to list
+    Person[] pArr = new Person[] {TestingPerson,TestingPerson2}; //Create array containing all Persons
+    assertArrayEquals(pArr,persons.toArray()); //Check if both arrays are equivalent
   }
 
   @Test
+  //Test the add method
   void add() {
-    persons.add(TestingPerson);
-    assertEquals(persons.size(),1);
+    persons.add(TestingPerson); //Add Person to list
+    assertEquals(persons.size(),1); //Check that the size of the new list contains one Person
   }
 
   @Test
   void set() {
-    persons.add(TestingPerson);
-    persons.set(0,TestingPerson2);
+    persons.add(TestingPerson); //Add Person to list
+    persons.set(0,TestingPerson2); //Replace the 1st element in list with new Person
+    //Check that the list now contains the second Person object instead of the first Person
     assertEquals("test2",persons.get(0).getFirstName());
   }
 
   @Test
   void remove() {
-    persons.add(TestingPerson);
-    persons.remove(0);
-    assertEquals(0,persons.size());
+    persons.add(TestingPerson); //Add Person to list
+    persons.remove(0); //Remove the Person that was just added
+    assertEquals(0,persons.size()); //Check that there are now no elements in the list
   }
 
   @ParameterizedTest
   @ValueSource(ints = {0,1})
   void get(int index) {
-    persons.add(TestingPerson);
-    persons.add(TestingPerson2);
+    persons.add(TestingPerson); //Add Person 1 to list
+    persons.add(TestingPerson2); //Add Person 2 to list
 
+    //If we are testing 0, we should get the first person in the list
+    //If we are testing 1, we should get the second person in the list
     if(index == 0){
       assertEquals(TestingPerson,persons.get(index));
     }
@@ -65,36 +71,36 @@ class AddressBookTest {
 
   @Test
   void clear() {
-    persons.add(TestingPerson);
-    persons.clear();
-    assertEquals(0,persons.size());
+    persons.add(TestingPerson); //Add Person to list
+    persons.clear(); //Clear the list
+    assertEquals(0,persons.size()); //Check that list contains 0 elements
   }
 
   @Test
   void getRowCount() {
-    persons.add(TestingPerson);
-    persons.add(TestingPerson2);
+    persons.add(TestingPerson); //Add Person 1 to list
+    persons.add(TestingPerson2); //Add Person 2 to list
 
-    assertEquals(2,persons.size());
+    assertEquals(2,persons.size()); //Check that 2 Persons were added to list
   }
 
   @Test
   void getColumnCount() {
-    assertEquals(7,Person.fields.length);
+    assertEquals(7,Person.fields.length); //Check that the Person has 7 attributes to it
   }
 
   @Test
   void getValueAt() {
-    int row = 0, col = 4;
-    persons.add(TestingPerson);
+    int row = 0, col = 4; //Take an arbitrary value
+    persons.add(TestingPerson); //Add Person to list
 
-    assertEquals("Test",persons.get(row).getField(col));
+    assertEquals("Test",persons.get(row).getField(col)); //Check we get the right value at that row, col input
   }
 
   @Test
   void getColumnName() {
-    int col = 3;
+    int col = 3; //Take arbitrary column
 
-    assertEquals("City",Person.fields[col]);
+    assertEquals("City",Person.fields[col]); //Check the arbitrary column matches
   }
 }
--- GUI/PersonTest.java
+++ GUI/PersonTest.java
@@ -11,6 +11,7 @@ class PersonTest {
 
   private Person TestingPerson;
 
+  //Create a new Person before each testing method is executed
   @BeforeEach
   void setUp(){
     TestingPerson = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
@@ -18,72 +19,69 @@ class PersonTest {
 
   @Test
   void getFirstName() {
-    //
-    String firstName = TestingPerson.getFirstName();
-    assertEquals("test",firstName);
+    String firstName = TestingPerson.getFirstName(); //Get First name of Person
+    assertEquals("test",firstName); //Check first name matches
   }
 
   @Test
   void getLastName() {
     
-    String lastName = TestingPerson.getLastName();
-    assertEquals("test",lastName);
+    String lastName = TestingPerson.getLastName(); //Get last name of Person
+    assertEquals("test",lastName); //Check last name matches
   }
 
   @Test
   void getAddress() {
     
-    String address = TestingPerson.getAddress();
-    assertEquals("123 Test",address);
+    String address = TestingPerson.getAddress(); //Get address  of Person
+    assertEquals("123 Test",address);//Check address matches
   }
 
   @Test
   void getCity() {
     
-    String city = TestingPerson.getCity();
-    assertEquals("Test",city);
+    String city = TestingPerson.getCity(); //Get city of Person
+    assertEquals("Test",city); //Check city matches
   }
 
   @Test
   void getState() {
     
-    String state = TestingPerson.getState();
-    assertEquals("Test",state);
+    String state = TestingPerson.getState(); //Get state of Person
+    assertEquals("Test",state); //Check state matches
   }
 
   @Test
   void getZip() {
     
-    String zip = TestingPerson.getZip();
-    assertEquals("Test",zip);
+    String zip = TestingPerson.getZip(); //Get zip of Person
+    assertEquals("Test",zip); //Check zip matches
   }
 
   @Test
   void getPhone() {
     
-    String phone = TestingPerson.getPhone();
-    assertEquals("Test",phone);
+    String phone = TestingPerson.getPhone(); //Get phone of Person
+    assertEquals("Test",phone); //Check phone matches
 
   }
 
-//  @Test
-//  void toString() {
-//  }
 
   @Test
   void containsString() {
-    String test = "test";
-    Pattern pa = Pattern.compile(Pattern.quote(test), Pattern.CASE_INSENSITIVE);
-    Person person = new Person("test","test","123 Test", "Test", "Test", "Test", "Test");
+    String test = "test"; //Vlaue to test whether or not the Person contains this attribute
+    Pattern pa = Pattern.compile(Pattern.quote(test), Pattern.CASE_INSENSITIVE); //Setup pattern
+
+    //Check each attribute of Person to see if there is a match
     boolean found =
-        pa.matcher(person.getFirstName()).find()
-            || pa.matcher(person.getLastName()).find()
-            || pa.matcher(person.getAddress()).find()
-            || pa.matcher(person.getCity()).find()
-            || pa.matcher(person.getState()).find()
-            || pa.matcher(person.getZip()).find()
-            || pa.matcher(person.getPhone()).find();
-    assertTrue(found);
+        pa.matcher(TestingPerson.getFirstName()).find()
+            || pa.matcher(TestingPerson.getLastName()).find()
+            || pa.matcher(TestingPerson.getAddress()).find()
+            || pa.matcher(TestingPerson.getCity()).find()
+            || pa.matcher(TestingPerson.getState()).find()
+            || pa.matcher(TestingPerson.getZip()).find()
+            || pa.matcher(TestingPerson.getPhone()).find();
+    assertTrue(found); //Return true if we found at least one value
 
   }
 
@@ -93,27 +91,28 @@ class PersonTest {
     
     switch (field) {
       case 0:
-        assertEquals("test",TestingPerson.getFirstName());
+        assertEquals("test",TestingPerson.getFirstName()); //Check first name
         break;
       case 1:
-        assertEquals("test",TestingPerson.getLastName());
+        assertEquals("test",TestingPerson.getLastName()); //Check last name
         break;
       case 2:
-        assertEquals("123 Test",TestingPerson.getAddress());
+        assertEquals("123 Test",TestingPerson.getAddress()); //Check address
         break;
       case 3:
-        assertEquals("Test",TestingPerson.getCity());
+        assertEquals("Test",TestingPerson.getCity()); //Check city
         break;
       case 4:
-        assertEquals("Test",TestingPerson.getState());
+        assertEquals("Test",TestingPerson.getState()); //Check state
         break;
       case 5:
-        assertEquals("Test",TestingPerson.getZip());
+        assertEquals("Test",TestingPerson.getZip()); //Check zip
         break;
       case 6:
-        assertEquals("Test",TestingPerson.getPhone());
+        assertEquals("Test",TestingPerson.getPhone()); //Check phone
         break;
       default:
+        //Out of bounds, check we throw an IllegalArgumentException
         assertThrows(IllegalArgumentException.class, () -> {
           TestingPerson.getField(field);
         });
